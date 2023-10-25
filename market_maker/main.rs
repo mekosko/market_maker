90bc6349bdfa9c8f66cb4840da12b2e748192365
mekosko <135838138+mekosko@users.noreply.github.com>
2023-10-25 16:11:12 +0500

    Reject abigen

--- market_maker/main.rs
+++ market_maker/main.rs
@@ -2,10 +2,12 @@ use uniswap::{UniswapV2Factory, UniswapV2Pool};
 
 pub mod uniswap;
 
+#[derive(Clone, Debug)]
 pub enum MarketMakerFactory {
 	UniswapV2Factory(UniswapV2Factory),
 }
 
+#[derive(Clone, Debug)]
 pub enum MarketMakerPool {
 	UniswapV2Pool(UniswapV2Pool),
 }
--- market_maker/uniswap.rs
+++ market_maker/uniswap.rs
@@ -1,13 +1,22 @@
-use ethers_contract::{abigen, EthLogDecode};
-use ethers_core::{abi, abi::RawLog, types::H160};
+use ethers_contract::EthEvent;
+use ethers_core::{
+	abi::{AbiError, RawLog},
+	types::{Address, H160, U256},
+};
 use serde::{Deserialize, Serialize};
 
-abigen!(
-	IUniswapV2Factory,
-	"[event PairCreated(address indexed token0, address indexed token1, address pair, uint256)]";
-);
+#[derive(Clone, Debug, Default, EthEvent)]
+#[ethevent(abi = "PairCreated(address,address,address,uint256)")]
+pub struct PairCreatedEvent {
+	#[ethevent(indexed)]
+	pub token_0: Address,
+	#[ethevent(indexed)]
+	pub token_1: Address,
+	pub address: Address,
+	pub count: U256,
+}
 
-type Result<T1> = std::result::Result<T1, abi::AbiError>;
+type Result<T1> = std::result::Result<T1, AbiError>;
 
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
 pub struct UniswapV2Factory {
@@ -23,32 +32,32 @@ impl UniswapV2Factory {
 
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
 pub struct UniswapV2Pool {
-	pub address: H160,
 	pub token_0: H160,
 	pub token_1: H160,
+	pub address: H160,
 }
 
 impl UniswapV2Pool {
-	pub fn new(address: H160, token_0: H160, token_1: H160) -> Self {
+	pub fn new(token_0: H160, token_1: H160, address: H160) -> Self {
 		Self {
-			address,
 			token_0,
 			token_1,
+			address,
 		}
 	}
 
 	pub fn new_from_event(event: &RawLog) -> Result<Self> {
-		let PairCreatedFilter {
+		let PairCreatedEvent {
 			token_0,
 			token_1,
-			pair,
+			address,
 			..
-		} = PairCreatedFilter::decode_log(event)?;
+		} = PairCreatedEvent::decode_log(event)?;
 
 		Ok(UniswapV2Pool {
-			address: pair,
 			token_0,
 			token_1,
+			address,
 		})
 	}
 }
